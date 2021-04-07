4410919a1fa77d0f5f42ab0f15eb03c52b2c419d
GeonHeeAhn <justamoment@ewhain.net>
2021-04-08 06:32:24 +0900

    feat : search용 버튼, input 박스 구현

--- src/main/Index.js
+++ src/main/Index.js
@@ -5,6 +5,7 @@ const Container = styled.div`
   display: flex;
   flex-direction: row;
   height: 100%;
+  width: 100%;
 `;
 
 function Index() {
--- src/main/InfoScreen.js
+++ src/main/InfoScreen.js
@@ -1,6 +1,7 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import FriendsList from './FriendsList';
 import Profile from './Profile';
+import {React, useState} from 'react';
 
 const Screen = styled.div`
     display: flex;
@@ -8,14 +9,52 @@ const Screen = styled.div`
     overflow-y: auto;
     overflow-x: hidden;
     height: 100%;
+    width: 100%;
     margin-left: 30px;
     margin-top: 10px;
 `;
+const Header = styled.div`
+    align-content: center;
+    display: flex;
+    flex-direction:row;
+    justify-content: space-between;
+    width:100%;
+`;
+const SearchButton = styled.button`
+    margin-top: 15px;
+    height: 30px;
+    width: 30px;
+    border: none;
+    :focus{
+        border: none;
+        outline: none;
+    }
+`;
 
-export default function InfoScreen(){
+const Input = styled.input`
+    width: 90%;
+    margin: 10px;
+    display: none;
+    ${(props) =>
+        props.isVisible &&
+        css` display: inherit;`
+    }
+`;
+
+export default function InfoScreen(props){
+    const [isVisibleSearch, setIsVisibleSearch] = useState(false);
+    function toggleVisibleSearch(){
+        setIsVisibleSearch(!isVisibleSearch);
+        
+    };
+
     return(
         <Screen>
-            <h3>친구목록</h3>
+            <Header>
+                <h3>친구목록</h3>
+                <SearchButton onClick={toggleVisibleSearch}>Search</SearchButton>
+            </Header>
+            <Input isVisible ={isVisibleSearch}/>
             <Profile/>
             <FriendsList/>
         </Screen>
